7727e08428407c213b2322752c60b7e5876a784d
zhxhdreams <zhxhdreams@outlook.com>
2019-02-25 01:38:42 +0800

    增加单词树组件

---
+++ WordTree.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Author: hai  
+# @Date: 2017-06-03 10:21:48
+
+__author__ = 'hai'
+
+
+class WordNode(object):
+
+    def __init__(self, value):
+        self.value = value
+        self.next = []
+        self.isWord = False
+        self.type = []
+
+    def find(self, value):
+        for item in self.next:
+            if item.value == value:
+                return item
+        return None
+
+    def add(self, value):
+        temp = self.find(value)
+        if temp is None:
+            temp = WordNode(value)
+            self.next.append(temp)
+        return temp
+
+
+class WordTree(object):
+
+    def __init__(self):
+        self.wordTree = []
+
+    def find(self, char):
+        for node in self.wordTree:
+            if node.value == char:
+                return node
+        return None
+
+    def addWordtoTree(self, word, wtype):
+        length = len(word)
+        parrent = None
+        for i, ch in enumerate(word):
+            if parrent is None:
+                parrent = self.find(ch)
+                if parrent is None:
+                    parrent = WordNode(ch)
+                    self.wordTree.append(parrent)
+            else:
+                parrent = parrent.add(ch)
+            if i == (length - 1):
+                parrent.isWord = True
+                if wtype not in parrent.type:
+                    parrent.type.append(wtype)
+
+
+if __name__ == '__main__':
+    print('The Module WordTree~')
